193642f6e77501bf52af1cf6ddd7e6d6a50ea6b4
Donistr <deniselag@mail.ru>
2023-04-29 19:18:03 +0400

    Added docstrings and tqdm to the brute.py file

--- brute.py
+++ brute.py
@@ -1,9 +1,16 @@
 from hashlib import sha1
 import multiprocessing as mp
 from typing import Optional
+from tqdm import tqdm
 
 
 def is_card_number_right(right_hash: str, card_number: str) -> bool:
+    """
+    function checks if the card number matches the given hash
+    :param right_hash: given hash
+    :param card_number: card number to check
+    :return: True - if the card number matches the given hash, False - otherwise
+    """
     hash = sha1(card_number.encode()).hexdigest()
     if hash == right_hash:
         return True
@@ -11,12 +18,20 @@ def is_card_number_right(right_hash: str, card_number: str) -> bool:
 
 
 def brute_card_number(right_hash: str, last_numbers: str, bins: list, cores: int = mp.cpu_count()) -> Optional[str]:
+    """
+    function tries to find a card number that matches the given hash
+    :param right_hash: given hash
+    :param last_numbers: last 4 digits of the card number
+    :param bins: bins
+    :param cores: number of cores
+    :return: card number - if found, None - if not found
+    """
     args = list()
     for i in range(1000000):
         for bin in bins:
             args.append((right_hash, f'{bin}{i}{last_numbers}'))
     with mp.Pool(processes=cores) as p:
-        for index, result in enumerate(p.starmap(is_card_number_right, args)):
+        for index, result in enumerate(p.starmap(is_card_number_right, tqdm(args, ncols=120))):
             if result:
                 p.terminate()
                 return args[index][-1]
